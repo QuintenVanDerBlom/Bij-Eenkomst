71e758238411a344eab1a758f8f3328c6712d631
Quinten <quinblom321@gmail.com>
2025-06-02 11:06:41 +0200

    Fix

--- server-side/models/Entry.js
+++ server-side/models/Entry.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const entrySchema = new mongoose.Schema({
     category_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
     sub_category_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Subcategory', required: true },
-    created_by: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    created_by: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },
     title: { type: String, required: true, maxlength: 150, unique: true },
     description: { type: String, required: true },
 });
