b0fd32b2aedce2e9c8567f397287fc33d7dcac54
Quinten <quinblom321@gmail.com>
2025-06-02 10:36:08 +0200

    Express Setup

---
+++ server-side/models/Entry.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const entrySchema = new mongoose.Schema({
+    category_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
+    sub_category_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Subcategory', required: true },
+    created_by: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    title: { type: String, required: true, maxlength: 150, unique: true },
+    description: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Entry', entrySchema);
